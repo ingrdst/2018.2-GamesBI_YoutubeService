4fc6c06e3e763ccb7f6111235c333c75032978c0
Felipe Osório <felipe.osorio72@outlook.com.br>
2018-09-02 21:53:41 -0300

    [ADD] fga-eps-mds/2018.2-GamesBI#75 Add mongodb config

--- YoutubeService/settings.py
+++ YoutubeService/settings.py
@@ -73,13 +73,13 @@ WSGI_APPLICATION = 'YoutubeService.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
+DATABASES = { 
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'djongo',
+        'NAME': 'mongo',
+        'HOST': 'db'
+    }   
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
